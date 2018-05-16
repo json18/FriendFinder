ac6457e855954ae6e3a804a3f2fb45a9d0bbec0e
Jessica Son <jessicason@Jessicas-MBP.hsd1.ca.comcast.net>
2018-05-16 06:42:41 -0700

    added some html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,23 +1,14 @@
 var friendsData = require("../data/friends");
 
-
-app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-  });
-  
-app.post("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
-  });
-
-
 module.exports = function(app) {
-    app.get("/api/tables", function(req, res) {
-      res.json(tableData);
-    });
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
   
-    app.get("/api/waitlist", function(req, res) {
-      res.json(waitListData);
-    });
-  
+  app.post("/api/friends", function(req, res) {
+    tableData.push(req.body);
+  });
 
-    
+};
+
+
